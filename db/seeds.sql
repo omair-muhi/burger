fa29eea4f5a562c834734d99959bc82d119604b4
Omair Muhi <omairmuhi@icloud.com>
2021-01-12 08:21:04 -0500

    Subject: Add 4 records via seeds file Problem: NA Solution: NA Note: NA

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Big Mac", false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Zinger", false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("McArabia", false);
