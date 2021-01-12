b9220ad6f3c3bf89b48429c0eec6b846d60e0256
Omair Muhi <omairmuhi@icloud.com>
2021-01-12 08:48:38 -0500

    Subject: Rename public functions Problem: NA Solution: NA Note: NA

--- config/orm.js
+++ config/orm.js
@@ -38,7 +38,7 @@ const objToSql = (ob) => {
 
 // Object for all our SQL statement functions.
 const orm = {
-    all(tableInput, cb) {
+    selectAll(tableInput, cb) {
         const queryString = `SELECT * FROM ${tableInput};`;
         connection.query(queryString, (err, result) => {
             if (err) {
@@ -47,7 +47,7 @@ const orm = {
             cb(result);
         });
     },
-    create(table, cols, vals, cb) {
+    insertOne(table, cols, vals, cb) {
         let queryString = `INSERT INTO ${table}`;
 
         queryString += ' (';
@@ -68,7 +68,7 @@ const orm = {
         });
     },
     // An example of objColVals would be {name: panther, sleepy: true}
-    update(table, objColVals, condition, cb) {
+    updateOne(table, objColVals, condition, cb) {
         let queryString = `UPDATE ${table}`;
 
         queryString += ' SET ';
@@ -85,7 +85,7 @@ const orm = {
             cb(result);
         });
     },
-    delete(table, condition, cb) {
+    deleteOne(table, condition, cb) {
         let queryString = `DELETE FROM ${table}`;
         queryString += ' WHERE ';
         queryString += condition;
