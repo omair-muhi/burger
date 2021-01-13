868a84939c5fcda620f8accb2ff50fa2b461ffbe
Omair Muhi <omairmuhi@icloud.com>
2021-01-13 18:54:00 -0500

    Subject: Enable handlebars engine Problem: NA Solution: NA Note: NA

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
 const routes = require('./controllers/burgers_controller.js');
