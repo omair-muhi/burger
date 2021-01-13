24884bf73e46f6e78179e3cc3b630db33fe0b417
Omair Muhi <omairmuhi@icloud.com>
2021-01-13 18:54:56 -0500

    Subject: Seed DB with default value of false for devoured Problem: NA Solution: NA Note: NA

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
 USE burgers_db;
 
-INSERT INTO burgers(burger_name, devoured)
-VALUES ("Big Mac", false);
+INSERT INTO burgers(burger_name)
+VALUES ("Big Mac");
 
-INSERT INTO burgers(burger_name, devoured)
-VALUES ("Zinger", false);
+INSERT INTO burgers(burger_name)
+VALUES ("Zinger");
 
-INSERT INTO burgers(burger_name, devoured)
-VALUES ("McArabia", false);
+INSERT INTO burgers(burger_name)
+VALUES ("McArabia");
 
-INSERT INTO burgers(burger_name, devoured)
-VALUES ("Mushroom Melt", true);
+INSERT INTO burgers(burger_name)
+VALUES ("Mushroom Melt");
