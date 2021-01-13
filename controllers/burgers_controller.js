ea450c271e00f6c22e8ab4c31c16c1aada7a2d68
Omair Muhi <omairmuhi@icloud.com>
2021-01-13 18:55:51 -0500

    Subject: Partially working /index API route Problem: NA Solution: NA Note: NA

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,53 +7,58 @@ const router = express.Router();
 const burger = require('../models/burger.js');
 
 // Create all our routes and set up logic within those routes where required.
-router.get('/', (req, res) => {
-    burger.all((data) => {
-        const hbsObject = {
-            burgers: data,
-        };
-        console.log(hbsObject);
-        res.render('index', hbsObject);
-    });
+router.get('/index', (req, res) => {
+    // burger.all((data) => {
+    const hbsObject = {
+        burgers: [
+            { id: 1, name: "Big Mac", devoured: false },
+            { id: 2, name: "Zinger", devoured: false },
+            { id: 3, name: "McArabia", devoured: false },
+            { id: 4, name: "Mushroom Melt", devoured: false }
+        ],
+    };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
+    // });
 });
 
-router.post('/api/burgers', (req, res) => {
-    cat.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
-        // Send back the ID of the new quote
-        res.json({ id: result.insertId });
-    });
-});
+// router.post('/api/burgers', (req, res) => {
+//     cat.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+//         // Send back the ID of the new quote
+//         res.json({ id: result.insertId });
+//     });
+// });
 
-router.put('/api/burgers/:id', (req, res) => {
-    const condition = `id = ${req.params.id}`;
+// router.put('/api/burgers/:id', (req, res) => {
+//     const condition = `id = ${req.params.id}`;
 
-    console.log('condition', condition);
+//     console.log('condition', condition);
 
-    burger.update({
-            devoured: req.body.devoured,
-        },
-        condition,
-        (result) => {
-            if (result.changedRows === 0) {
-                // If no rows were changed, then the ID must not exist, so 404
-                return res.status(404).end();
-            }
-            res.status(200).end();
-        }
-    );
-});
+//     burger.update({
+//             devoured: req.body.devoured,
+//         },
+//         condition,
+//         (result) => {
+//             if (result.changedRows === 0) {
+//                 // If no rows were changed, then the ID must not exist, so 404
+//                 return res.status(404).end();
+//             }
+//             res.status(200).end();
+//         }
+//     );
+// });
 
-router.delete('/api/burgers/:id', (req, res) => {
-    const condition = `id = ${req.params.id}`;
+// router.delete('/api/burgers/:id', (req, res) => {
+//     const condition = `id = ${req.params.id}`;
 
-    burger.delete(condition, (result) => {
-        if (result.affectedRows === 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        }
-        res.status(200).end();
-    });
-});
+//     burger.delete(condition, (result) => {
+//         if (result.affectedRows === 0) {
+//             // If no rows were changed, then the ID must not exist, so 404
+//             return res.status(404).end();
+//         }
+//         res.status(200).end();
+//     });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
