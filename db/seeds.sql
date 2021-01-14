df8365e560a2f5bb1ec9472340bdc7a4c4888cd2
Omair Muhi <omairmuhi@icloud.com>
2021-01-14 08:33:17 -0500

    Subject: Default all burgers to not-devoured Problem: NA Solution: NA Note: NA

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
 USE burgers_db;
 
-INSERT INTO burgers(burger_name, devoured)
-VALUES ("Big Mac", true);
+INSERT INTO burgers(burger_name)
+VALUES ("Big Mac");
 
 INSERT INTO burgers(burger_name)
 VALUES ("Zinger");
 
-INSERT INTO burgers(burger_name, devoured)
-VALUES ("McArabia", true);
+INSERT INTO burgers(burger_name)
+VALUES ("McArabia");
 
 INSERT INTO burgers(burger_name)
 VALUES ("Mushroom Melt");
